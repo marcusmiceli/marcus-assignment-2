bb533436bd94676ac342a2f6471561f267da4c26
Marcus Miceli <marcusmiceli@shaw.ca>
2022-02-18 00:13:50 -0500

    Update Assignment2Application.java

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -1,10 +1,65 @@
 package com.coderscampus;
 
+import java.util.Scanner;
+
+import java.util.Random;
+
 public class Assignment2Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in); 
+		System.out.println("Pick a number between 1 and 100");
+		String userInput=scanner.nextLine();
+		Integer convertedInput=Integer.parseInt(userInput);
+		
+		Random random = new Random();
+		int answer = random.nextInt(100) + 1;
+		
+		int i = convertedInput;
+		while ((i < 1)
+			|| (i > 100)) {
+			Scanner newScanner = new Scanner(System.in);
+			System.out.println("Your guess is not between 1 and 100, please try again");
+			// This can be in a method
+			System.out.println("Pick a number between 1 and 100");
+			String newUserInput=newScanner.nextLine();
+			Integer newConvertedInput=Integer.parseInt(newUserInput);
+			i = newConvertedInput;
+			}
+		
+		
+		int counter = 1;
+		while (counter < 5) {
+			if (i < answer) {
+				Scanner newScanner = new Scanner(System.in);
+				System.out.println("Pick a higher number");
+				String newUserInput=newScanner.nextLine();
+				Integer newConvertedInput=Integer.parseInt(newUserInput);
+				i = newConvertedInput;
+				counter = counter + 1;
+			}
+			else if (i > answer) {
+				Scanner newScanner = new Scanner(System.in);
+				System.out.println("Pick a lower number");
+				String newUserInput=newScanner.nextLine();
+				Integer newConvertedInput=Integer.parseInt(newUserInput);
+				i = newConvertedInput;
+				counter = counter + 1;
+			}
+			else if (i == answer) {
+				System.out.println("You win!");
+				break;
+			}
+		}
+		
+		if (i != answer) {
+			System.out.println("You lose!");
+			System.out.println("The number to guess was: " + answer);
+		}
+			
+		}
+		
 
 	}
 
-}
