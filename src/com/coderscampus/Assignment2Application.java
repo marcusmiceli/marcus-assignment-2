5dce4235c243bc58c4dcc8468ef4a80d8261b8f9
Marcus Miceli <marcusmiceli@shaw.ca>
2022-02-21 18:26:29 -0500

    Update Assignment2Application.java

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -31,7 +31,9 @@ public class Assignment2Application {
 		
 		int counter = 1;
 		while (counter < 5) {
-			if (i < answer) {
+			if ((i < answer)
+				&& (i<=100)
+				&& (i>=1)){
 				Scanner newScanner = new Scanner(System.in);
 				System.out.println("Pick a higher number");
 				String newUserInput=newScanner.nextLine();
@@ -39,13 +41,25 @@ public class Assignment2Application {
 				i = newConvertedInput;
 				counter = counter + 1;
 			}
-			else if (i > answer) {
+			else if ((i > answer)
+			&& (i<=100)
+			&& (i>=1)){
 				Scanner newScanner = new Scanner(System.in);
 				System.out.println("Pick a lower number");
 				String newUserInput=newScanner.nextLine();
 				Integer newConvertedInput=Integer.parseInt(newUserInput);
 				i = newConvertedInput;
 				counter = counter + 1;
+			}
+			else if ((i < 1)
+			|| (i > 100)){
+				Scanner newScanner = new Scanner(System.in);
+				System.out.println("Your guess is not between 1 and 100, please try again");
+				// This can be in a method
+				System.out.println("Pick a number between 1 and 100");
+				String newUserInput=newScanner.nextLine();
+				Integer newConvertedInput=Integer.parseInt(newUserInput);
+				i = newConvertedInput;
 			}
 			else if (i == answer) {
 				System.out.println("You win!");
@@ -57,6 +71,10 @@ public class Assignment2Application {
 			System.out.println("You lose!");
 			System.out.println("The number to guess was: " + answer);
 		}
+		else if ((i == answer)
+				&& (counter==5)) {
+				System.out.println("You win!");
+			}
 			
 		}
 		
